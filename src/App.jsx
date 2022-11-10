802fd30a1456f3c1856a5205b57efa26357c50d2
James Osunkiyesi <jaydhales@gmail.com>
2022-11-10 21:42:29 +0100

    feat: Full Route Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import { useContext } from "react";
 import { MetaContext } from "./Contexts/MetaContext";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home.jsx";
+import Place from "./Pages/Place.jsx";
+import Header from "./Components/Header.jsx";
+import Footer from "./Components/Footer.jsx";
 
 function App() {
-  const { test } = useContext(MetaContext);
-  return <div className="App">This is {test}</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="place" element={<Place />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/Contexts/MetaContext.jsx
+++ src/Contexts/MetaContext.jsx
@@ -3,16 +3,7 @@ import { createContext, useEffect, useState } from "react";
 const MetaContext = createContext();
 
 const MetaContextProvider = ({ children }) => {
-  const [test, setTest] = useState("testing 1,2,3");
-  return (
-    <MetaContext.Provider
-      value={{
-        test,
-      }}
-    >
-      {children}
-    </MetaContext.Provider>
-  );
+  return <MetaContext.Provider value={{}}>{children}</MetaContext.Provider>;
 };
 
 export { MetaContext, MetaContextProvider };
