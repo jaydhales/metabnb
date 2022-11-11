2ab7a4c57a9b97fd7b0e784ff12bba4ef7043ac1
James Osunkiyesi <jaydhales@gmail.com>
2022-11-11 23:42:25 +0100

    feat: added footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,58 @@
+import SvgImage from "../Components/SvgImage.jsx";
+import Logo from "../assets/footer-logo.svg";
+import facebook from "../assets/socials/facebook.svg";
+import instagram from "../assets/socials/instagram.svg";
+import twitter from "../assets/socials/twitter.svg";
+
+import "../styles/Footer.css";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="layout pt-14">
+      <div className="footer-main">
+        <div className="footer-icons">
+          <SvgImage data={Logo} />
+          <div className="socials">
+            <SvgImage data={facebook} />
+            <SvgImage data={instagram} />
+            <SvgImage data={twitter} />
+          </div>
+        </div>
 
-export default Footer
+        <div className="footer-content">
+          <div className="links">
+            <p>Community</p>
+            <nav>
+              <a href="#">NFT</a>
+              <a href="#">Tokens</a>
+              <a href="#">Landlords</a>
+              <a href="#">Discord</a>
+            </nav>
+          </div>
+          <div className="links">
+            <p>Places</p>
+            <nav>
+              <a href="#">Castle</a>
+              <a href="#">Farms</a>
+              <a href="#">Beach</a>
+              <a href="#">Learn more</a>
+            </nav>
+          </div>
+          <div className="links">
+            <p>About us</p>
+            <nav>
+              <a href="#">Road map</a>
+              <a href="#">Creators</a>
+              <a href="#">Career</a>
+              <a href="#">Learn more</a>
+            </nav>
+          </div>
+        </div>
+      </div>
+
+      <p className="mt-8 mb-4">© 2022 Metabnb</p>
+    </footer>
+  );
+};
+
+export default Footer;
