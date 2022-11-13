ecb76de47f2f7907874e007730a495b755a2fc80
James Osunkiyesi <jaydhales@gmail.com>
2022-11-13 20:23:32 +0100

    refactor: changed svg to img

--- src/Components/IntroSect.jsx
+++ src/Components/IntroSect.jsx
@@ -1,10 +1,8 @@
-/* eslint-disable no-unused-vars */
-import SvgImage from "./SvgImage";
 import Search from "./Search";
-import image1 from "../assets/intro/image 1.svg";
-import image2 from "../assets/intro/image 2.svg";
-import image3 from "../assets/intro/image 3.svg";
-import image4 from "../assets/intro/image 4.svg";
+import image1 from "../assets/intro/image 1.png";
+import image2 from "../assets/intro/image 2.png";
+import image3 from "../assets/intro/image 3.png";
+import image4 from "../assets/intro/image 4.png";
 
 const IntroSect = () => {
   return (
